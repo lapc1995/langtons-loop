12a7538b3907f934581964afc24679a5d8575030
Luís Carvalho <lapc19@hotmail.com>
2019-03-10 17:47:28 +0000

    Updates

---
+++ precache-manifest.765474dd99795790e259ca6bda0b1fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9cde4fbf5ec552bebdf",
+    "url": "/langtons-loop/static/js/runtime~main.4b5aadce.js"
+  },
+  {
+    "revision": "ef4dd1d9930d3f8aa503",
+    "url": "/langtons-loop/static/js/main.1aede3c7.chunk.js"
+  },
+  {
+    "revision": "6d8c8a330f958afe78e3",
+    "url": "/langtons-loop/static/js/2.17621314.chunk.js"
+  },
+  {
+    "revision": "ef4dd1d9930d3f8aa503",
+    "url": "/langtons-loop/static/css/main.15b26ce1.chunk.css"
+  },
+  {
+    "revision": "1c5c8b67b3a63d33cd655a7382b8fb50",
+    "url": "/langtons-loop/index.html"
+  }
+];
